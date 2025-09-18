bf1a288e2b9f2adcb897b53a614133cd9b9e9972
Meet Thakar <mvthakar@outlook.com>
2025-09-18 13:13:18 +0530

    Start login UI

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+def show_login_page(request: HttpRequest):
+    return render(request, 'login.html')
+
+def show_signup_page(request: HttpRequest):
+    return render(request, 'signup.html')
