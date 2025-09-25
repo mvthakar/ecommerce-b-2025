224575ed69f63d8859849859f1579c14d06a6bc7
Meet Thakar <mvthakar@outlook.com>
2025-09-25 11:38:54 +0530

    Complete logic for login and sign up

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.show_login_page),
-    path('signup/', views.show_signup_page),
+    path('login/', views.login),
+    path('signup/', views.signup),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,32 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-def show_login_page(request: HttpRequest):
-    return render(request, 'login.html')
+def login(request: HttpRequest):
+    if request.method == "GET":
+        return render(request, 'login.html')
 
-def show_signup_page(request: HttpRequest):
-    return render(request, 'signup.html')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    
+    if email != "admin@gmail.com" or password != "Admin@1234":
+        return HttpResponse("Wrong email or password")
+    
+    return HttpResponse("Logged in")
+
+def signup(request: HttpRequest):
+    if request.method == "GET":
+        return render(request, 'signup.html')
+
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    confirmPassword = request.POST.get('confirm-password')
+    
+    if password != confirmPassword:
+        return HttpResponse("Passwords do not match")
+
+    # Validate email format
+    # Validate password format
+    
+    # Same email check
+    
+    return HttpResponse("Signed up")
