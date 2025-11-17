913428ccfeff667efa78026078851544f522c872
Meet Thakar <mvthakar@outlook.com>
2025-11-17 10:10:16 +0530

    Complete login, sign up, log out; move home page to its own app

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import redirect, render
 from django.contrib.auth.hashers import make_password, check_password
 
@@ -7,91 +7,82 @@ from utils.validators import email_validator, password_validator
 
 
 def login(request: HttpRequest):
-    if request.method == "GET":
-        if request.COOKIES.get('email') is not None:
-            return redirect('/users/home')
-        
-        return render(request, 'login.html')
+  if request.method == "GET":
+    if request.COOKIES.get('email') is not None:
+      return redirect('home')
+    return render(request, 'login.html')
 
-    email = request.POST.get('email')
-    password = request.POST.get('password')
-    
-    user = User.objects.filter(email=email)
-    if user.count() == 0:
-        return render(request, 'login.html', {
-            'error': 'Wrong email or password'
-        })
+  email = request.POST.get('email')
+  password = request.POST.get('password')
+  
+  user = User.objects.filter(email=email)
+  if user.count() == 0:
+    return render(request, 'login.html', {
+      'error': 'Wrong email or password'
+    })
 
-    if not check_password(password, user.get().password_hash):
-        return render(request, 'login.html', {
-            'error': 'Wrong email or password'
-        })
-    
-    # Cookie
-    response = redirect("/users/home")
-    response.set_cookie('email', email)
-    
-    return response
+  if not check_password(password, user.get().password_hash):
+    return render(request, 'login.html', {
+      'error': 'Wrong email or password'
+    })
+  
+  # Cookie
+  response = redirect("home")
+  response.set_cookie('email', email)
+  
+  return response
 
 
 def signup(request: HttpRequest):
-    if request.method == "GET":
-        if request.COOKIES.get('email') is not None:
-            return redirect('/users/home')
-        
-        return render(request, 'signup.html')
-
-    email = request.POST.get('email')
-    password = request.POST.get('password')
-    confirmPassword = request.POST.get('confirm-password')
+  if request.method == "GET":
+    if request.COOKIES.get('email') is not None:
+      return redirect('home')
     
-    if password != confirmPassword:
-        return render(request, 'signup.html', {
-            'error': 'Passwords do no match'
-        })
-        
+    return render(request, 'signup.html')
 
-    is_email_valid = email_validator.validate(email)
-    if not is_email_valid:
-        return render(request, 'signup.html', {
-            'error': 'Email is invalid'
-        })
+  email = request.POST.get('email')
+  password = request.POST.get('password')
+  confirmPassword = request.POST.get('confirm-password')
+  
+  if password != confirmPassword:
+    return render(request, 'signup.html', {
+      'error': 'Passwords do no match'
+    })
+      
 
-    is_password_valid = password_validator.validate(password)
-    if not is_password_valid:
-        return render(request, 'signup.html', {
-            'error': "Password must be more than 8 characters and must contain at least one capital, small, number, and special character(s)"
-        })
-    
-    existing_email = User.objects.filter(email=email)
-    if existing_email.count() > 0:
-        return render(request, 'signup.html', {
-            'error': "Email is already taken"
-        })
+  is_email_valid = email_validator.validate(email)
+  if not is_email_valid:
+    return render(request, 'signup.html', {
+      'error': 'Email is invalid'
+    })
 
-    customer_role = Role.objects.get(name="Customer")
-    hashsed_password = make_password(password)
+  is_password_valid = password_validator.validate(password)
+  if not is_password_valid:
+    return render(request, 'signup.html', {
+      'error': "Password must be more than 8 characters and must contain at least one capital, small, number, and special character(s)"
+    })
+  
+  existing_email = User.objects.filter(email=email)
+  if existing_email.count() > 0:
+    return render(request, 'signup.html', {
+      'error': "Email is already taken"
+    })
 
-    # insert
-    User.objects.create(
-        email=email,
-        password_hash=hashsed_password,
-        role=customer_role
-    )
+  customer_role = Role.objects.get(name="Customer")
+  hashsed_password = make_password(password)
 
-    return redirect('/users/login')
+  # insert
+  User.objects.create(
+    email=email,
+    password_hash=hashsed_password,
+    role=customer_role
+  )
+
+  return redirect('login')
 
 
 def logout(request: HttpRequest):
-    response = redirect('/users/login')
-    response.delete_cookie('email')
-    
-    return response    
-
-
-def home(request: HttpRequest):
-    if request.COOKIES.get('email') is None:
-        return redirect('/users/login')
-    
-    return render(request, 'home.html')
-
+  response = redirect('login')
+  response.delete_cookie('email')
+  
+  return response
