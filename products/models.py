84c7025865a6e059204bd898477a69cec3721569
Meet Thakar <mvthakar@outlook.com>
2025-12-09 09:49:20 +0530

    Create admin site and register Category and Product models

--- products/models.py
+++ products/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
   name = models.CharField(max_length=255)
   price = models.FloatField()
   category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
